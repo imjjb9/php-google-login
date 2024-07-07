0b161c1f419ef05052e05395cbd4d5ad80b3ca1d
Jayjay <johnjuliusbaldia@gmail.com>
2024-07-07 14:11:11 +0800

    tested

--- welcome.php
+++ welcome.php
@@ -3,79 +3,83 @@ require_once 'config.php';
 
 // authenticate code from Google OAuth Flow
 if (isset($_GET['code'])) {
-  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-  $client->setAccessToken($token['access_token']);
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token['access_token']);
 
-  // get profile info
-  $google_oauth = new Google_Service_Oauth2($client);
-  $google_account_info = $google_oauth->userinfo->get();
-  $userinfo = [
-    'email' => $google_account_info['email'],
-    'first_name' => $google_account_info['givenName'],
-    'last_name' => $google_account_info['familyName'],
-    'gender' => $google_account_info['gender'],
-    'full_name' => $google_account_info['name'],
-    'picture' => $google_account_info['picture'],
-    'verifiedEmail' => $google_account_info['verifiedEmail'],
-    'token' => $google_account_info['id'],
-  ];
+    // get profile info
+    $google_oauth = new Google_Service_Oauth2($client);
+    $google_account_info = $google_oauth->userinfo->get();
+    $userinfo = [
+        'email' => $google_account_info['email'],
+        'first_name' => $google_account_info['givenName'],
+        'last_name' => $google_account_info['familyName'],
+        'gender' => $google_account_info['gender'],
+        'full_name' => $google_account_info['name'],
+        'picture' => $google_account_info['picture'],
+        'verifiedEmail' => $google_account_info['verifiedEmail'],
+        'token' => $google_account_info['id'],
+    ];
 
-  // checking if user is already exists in database
-  $sql = "SELECT * FROM users WHERE email ='{$userinfo['email']}'";
-  $result = mysqli_query($conn, $sql);
-  if (mysqli_num_rows($result) > 0) {
-    // user is exists
-    $userinfo = mysqli_fetch_assoc($result);
-    $token = $userinfo['token'];
-  } else {
-    // user is not exists
-    $sql = "INSERT INTO users (email, first_name, last_name, gender, full_name, picture, verifiedEmail, token) VALUES ('{$userinfo['email']}', '{$userinfo['first_name']}', '{$userinfo['last_name']}', '{$userinfo['gender']}', '{$userinfo['full_name']}', '{$userinfo['picture']}', '{$userinfo['verifiedEmail']}', '{$userinfo['token']}')";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
-      $token = $userinfo['token'];
-    } else {
-      echo "User is not created";
-      die();
+    // Check if the email domain is @student.pnm.edu.ph
+    $email_domain = substr(strrchr($userinfo['email'], "@"), 1);
+    if ($email_domain !== 'student.pnm.edu.ph') {
+        // Redirect to an error page or display an error message
+        echo "Access denied: Unauthorized domain.";
+        die();
     }
-  }
 
-  // save user data into session
-  $_SESSION['user_token'] = $token;
+    // checking if user is already exists in database
+    $sql = "SELECT * FROM users WHERE email ='{$userinfo['email']}'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // user exists
+        $userinfo = mysqli_fetch_assoc($result);
+        $token = $userinfo['token'];
+    } else {
+        // user does not exist
+        $sql = "INSERT INTO users (email, first_name, last_name, gender, full_name, picture, verifiedEmail, token) VALUES ('{$userinfo['email']}', '{$userinfo['first_name']}', '{$userinfo['last_name']}', '{$userinfo['gender']}', '{$userinfo['full_name']}', '{$userinfo['picture']}', '{$userinfo['verifiedEmail']}', '{$userinfo['token']}')";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            $token = $userinfo['token'];
+        } else {
+            echo "User could not be created.";
+            die();
+        }
+    }
+
+    // save user data into session
+    $_SESSION['user_token'] = $token;
 } else {
-  if (!isset($_SESSION['user_token'])) {
-    header("Location: index.php");
-    die();
-  }
+    if (!isset($_SESSION['user_token'])) {
+        header("Location: index.php");
+        die();
+    }
 
-  // checking if user is already exists in database
-  $sql = "SELECT * FROM users WHERE token ='{$_SESSION['user_token']}'";
-  $result = mysqli_query($conn, $sql);
-  if (mysqli_num_rows($result) > 0) {
-    // user is exists
-    $userinfo = mysqli_fetch_assoc($result);
-  }
+    // checking if user is already exists in database
+    $sql = "SELECT * FROM users WHERE token ='{$_SESSION['user_token']}'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // user exists
+        $userinfo = mysqli_fetch_assoc($result);
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
 </head>
-
 <body>
-  <img src="<?= $userinfo['picture'] ?>" alt="" width="90px" height="90px">
-  <ul>
-    <li>Full Name: <?= $userinfo['full_name'] ?></li>
-    <li>Email Address: <?= $userinfo['email'] ?></li>
-    <li>Gender: <?= $userinfo['gender'] ?></li>
-    <li><a href="logout.php">Logout</a></li>
-  </ul>
+    <img src="<?= $userinfo['picture'] ?>" alt="" width="90px" height="90px">
+    <ul>
+        <li>Full Name: <?= $userinfo['full_name'] ?></li>
+        <li>Email Address: <?= $userinfo['email'] ?></li>
+        <li>Gender: <?= $userinfo['gender'] ?></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
 </body>
-
 </html>
